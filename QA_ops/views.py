b5212a7e97f3be52cf36d86df71d9e84626464e6
张伟伟(小懒虫) <zhangweiwei@uuzu.com>
2018-11-02 00:00:11 +0800

    功能研发中

--- QA_ops/views.py
+++ QA_ops/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+import time
+
+def index(request):
+    context = {}
+    context['hello'] = "hello,张伟伟！"
+    context['date'] = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+    print(context)
+    return render(request,'hello.html',context)
+def command(request):
+    request.encoding = 'utf-8'
+    info="serverIp"+request.GET['serverIp']+"/n"
+    info=info+"command"+request.GET['command']
+    print(info)
+    return HttpResponse("xx")
