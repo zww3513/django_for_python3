a44437571268eef338dacc7e1550797c45db3a0d
张伟伟(小懒虫) <zhangweiwei@uuzu.com>
2018-11-06 20:27:37 +0800

    编写远程执行命令接口，后续想改为post方法，加token认证

--- QA_ops/views.py
+++ QA_ops/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-import time
+import time,paramiko
+
+
+def ssh_command(hostname, port, user, password, cmd):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(hostname, port, user, password)
+    except:
+        return "连接建立失败，请检查服务器信息是否正确！！！"
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    result = stdout.readlines()
+    if not result:
+        result = stderr.readlines()
+    ssh.close()
+    result = ''.join(result)
+    result="<pre>"+result+"</pre>"
+    return result
 
 def index(request):
     context = {}
@@ -11,7 +28,10 @@ def index(request):
     return render(request, 'index.html', context)
 def command(request):
     request.encoding = 'utf-8'
-    info="serverIp="+request.GET['serverIp']+"<br>"
-    info=info+"command="+request.GET['command']
-    print(info)
+    hostname=request.GET.get("hostname")
+    port='22'
+    user='root'
+    password='123456'
+    cmd=request.GET.get("cmd")
+    info=ssh_command(hostname, port, user, password, cmd)
     return HttpResponse(info)
--- django_for_python3/urls.py
+++ django_for_python3/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from QA_ops import views
+from QA_ops import views as qa_ops_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
-    path("command/",views.command)
+    path('',qa_ops_views.index),
+path('command',qa_ops_views.command),
 ]
